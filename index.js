fd52ef4dc81b38bef8afa7374d3d79c3a3d9ddb8
Aidan Israel <aidandisrael@gmail.com>
2022-08-02 23:08:15 -0700

    cursor

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft(){
+    dodger.style.left = "100px";
+}
+
+function moveDodgerRight(){
+    dodger.style.left = "200px";
+}
